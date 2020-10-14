f40180ca614d96386b2adf2e760a37e23acc09d9
Arthur Gailes <agailes1@gmail.com>
2020-10-14 16:36:14 -0700

    make template compatible with starting an R package inside ti

--- code/R/setup.R
+++
@@ -1,9 +0,0 @@
-# the R code used to set up the root directory
-
-library(purrr)
-library(here)
-folder_names <- c("data","data/raw", "data/tidy","data/final", "references", "code", "code/R",
-  "analysis","analysis/reports","analysis/data","analysis/figures", 
-  "products","products/reports","products/data","products/figures")
-#sapply(folder_names, dir.create) # base R way
-map(folder_names, dir.create) #purrr-fect way
